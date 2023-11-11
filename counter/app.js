08744b26111086e66789ab722df16b0265bda912
Nguyễn Gia Bách <108968549+nguyengiabach1201@users.noreply.github.com>
2023-11-11 18:19:21 +0700

    Add files via upload

--- counter/app.js
+++ counter/app.js
@@ -5,5 +5,5 @@ setInterval(function () {
   const startOfYear = new Date(now.getFullYear(), 0, 1);
   const secondsPassed = (now - startOfYear) / 1000;
   // console.log(Math.trunc(secondsPassed * 0.2853881278538813));
-  document.getElementById("number").innerHTML = String(Math.trunc(secondsPassed * 0.2853881278538813).toString()).replace(/(.)(?=(\d{3})+$)/g,'$1,')
+  document.getElementById("number").innerHTML = String(Math.trunc(secondsPassed * 0.2851988815).toString()).replace(/(.)(?=(\d{3})+$)/g,'$1,')
 }, 100);
