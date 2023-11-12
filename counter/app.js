52a05e08eb73beee37e00addfd6306afdccc8878
Nguyễn Gia Bách <108968549+nguyengiabach1201@users.noreply.github.com>
2023-11-12 09:43:03 +0700

    Delete counter/app.js

--- counter/app.js
+++
@@ -1,9 +0,0 @@
-
-
-setInterval(function () {
-  const now = new Date();
-  const startOfYear = new Date(now.getFullYear(), 0, 1);
-  const secondsPassed = (now - startOfYear) / 1000;
-  // console.log(Math.trunc(secondsPassed * 0.2853881278538813));
-  document.getElementById("number").innerHTML = String(Math.trunc(secondsPassed * 0.2851988815).toString()).replace(/(.)(?=(\d{3})+$)/g,'$1,')
-}, 100);
